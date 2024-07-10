7e222daa7ca9334a0259d2b16e9c316405938c2b
naama2399 <136565329+naama2399@users.noreply.github.com>
2024-07-10 15:51:24 +0300

    Update dash1.py

--- dash1.py
+++ dash1.py
@@ -45,8 +45,8 @@ def plot_map(df, col, pal):
         hover_name="Country",
         title='ART Coverage by Country',
         color_continuous_scale=pal,
-        width=1200,  # Increase width
-        height=1200  # Increase height
+        width=800,  # Increase width
+        height=600  # Increase height
     )
 
     fig.update_layout(
