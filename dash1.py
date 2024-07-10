db15e19009b8ddb24c16d8b8ff0a37814100f828
naama2399 <136565329+naama2399@users.noreply.github.com>
2024-07-10 15:49:05 +0300

    Update dash1.py

--- dash1.py
+++ dash1.py
@@ -39,8 +39,7 @@ def plot_map(df, col, pal):
     # Create choropleth map using Plotly Express
     fig = px.choropleth(df, locations="Country", locationmode='country names',
                         color=col, hover_name="Country",
-                        title='ART Coverage by Country', color_continuous_scale=pal, width=1200, height=600)
-    return fig
+300    return fig
 
 def update_cumulative_incidence_curve():
     fig_cumulative_incidence_curve = go.Figure()
