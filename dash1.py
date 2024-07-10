727bae2ade5c1ac32da194ac5cc504d19563d4fb
naama2399 <136565329+naama2399@users.noreply.github.com>
2024-07-10 17:57:45 +0300

    Update dash1.py

--- dash1.py
+++ dash1.py
@@ -42,7 +42,7 @@ def plot_map(df, col, pal):
     # Create choropleth map using Plotly Express
     fig = px.choropleth(df, locations="Country", locationmode='country names',
                         color=col, hover_name="Country",
-                        title='ART Coverage by Country', color_continuous_scale=pal, width=1500, height=1200)
+                        title='ART Coverage by Country', color_continuous_scale=pal, width=1500, height=800)
     fig.update_layout(
         autosize=False,
         width=1500,
@@ -51,9 +51,10 @@ def plot_map(df, col, pal):
         title_font_size=35,  # Adjust the title font size here
         coloraxis_colorbar=dict(
             title="Reported number of people receiving ART",
-            thicknessmode="pixels", thickness=15,
-            lenmode="pixels", len=300,
+            thicknessmode="pixels", thickness=30,  # Increase the thickness for enlargement
+            lenmode="pixels", len=400,  # Increase the length
             yanchor="middle", y=0.5,
+            xanchor="left", x=1.02,  # Move the color bar closer to the map
             ticks="outside"
         )
     )
