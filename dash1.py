d602c3ae8bef5f29e4caeca61df28e4aea29760e
naama2399 <136565329+naama2399@users.noreply.github.com>
2024-07-10 17:47:03 +0300

    Update dash1.py

--- dash1.py
+++ dash1.py
@@ -47,8 +47,8 @@ def plot_map(df, col, pal):
         autosize=False,
         width=1500,
         height=800,
-        margin={"r":0,"t":100,"l":0,"b":0},  # Adjusted margin settings
-        title_font_size=24,  # Adjust the title font size here
+        margin={"r":0,"t":50,"l":0,"b":0},  # Adjusted margin settings
+        title_font_size=48,  # Adjust the title font size here
         coloraxis_colorbar=dict(
             title="Reported number of people receiving ART",
             thicknessmode="pixels", thickness=15,
