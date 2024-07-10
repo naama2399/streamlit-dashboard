30f87d52114b67e68f667604981f8d89e2ce676f
naama2399 <136565329+naama2399@users.noreply.github.com>
2024-07-10 15:52:52 +0300

    Update dash1.py

--- dash1.py
+++ dash1.py
@@ -37,28 +37,9 @@ def plot_map(df, col, pal):
     df[col] = pd.to_numeric(df[col], errors='coerce')
 
     # Create choropleth map using Plotly Express
-    fig = px.choropleth(
-        df,
-        locations="Country",
-        locationmode='country names',
-        color=col,
-        hover_name="Country",
-        title='ART Coverage by Country',
-        color_continuous_scale=pal,
-        width=800,  # Increase width
-        height=600  # Increase height
-    )
-
-    fig.update_layout(
-        autosize=False,
-        margin=dict(l=50, r=50, t=50, b=50),  # Adjust margins
-        geo=dict(
-            showframe=False,
-            showcoastlines=False,
-            projection_type='equirectangular'
-        )
-    )
-
+    fig = px.choropleth(df, locations="Country", locationmode='country names',
+                        color=col, hover_name="Country",
+                        title='ART Coverage by Country', color_continuous_scale=pal, width=1200)
     return fig
 
 def update_cumulative_incidence_curve():
@@ -89,13 +70,11 @@ def update_cumulative_incidence_curve():
         ))
 
     # Update layout of the figure
-    fig_cumulative_incidence_curve.update_layout(
-        title='Cumulative Incidence Curves by ART Protocol',
-        xaxis_title='Time (days)',
-        yaxis_title='Cumulative Proportion of Deaths'
-    )
+    fig_cumulative_incidence_curve.update_layout(title='Cumulative Incidence Curves by ART Protocol',
+                                                 xaxis_title='Time (days)',
+                                                 yaxis_title='Cumulative Proportion of Deaths')
 
-    return fig
+    return fig_cumulative_incidence_curve
 
 # Streamlit app
 # Add a main image with a larger width
